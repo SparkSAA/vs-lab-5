1a27994f1a9a73872818d4f134154e52f4b8842d
Anton <spark270907@gmail.com>
2025-12-06 01:12:26 +0900

    first task of the lab done

---
+++ task2.sql
@@ -0,0 +1,22 @@
+create table institutes
+(
+	id serial primary key,
+	name varchar(1024) not null
+);
+
+create table groups
+(
+	id serial primary key,
+	name varchar(2048) not null,
+	short_name varchar (64) not null unique,
+	year integer,
+	institute_id integer not null
+);
+
+create table students
+(
+	surname varchar(512) not null,
+	name varchar(512) not null,
+	father_name varchar(512),
+	group_id integer not null
+);
